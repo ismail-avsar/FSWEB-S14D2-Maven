96340df79e0dc6e1861da37f94dd3b19eab3d668
ismail <i.avsar@outlook.com>
2026-01-30 16:30:52 +0300

    Proje tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
-package org.example;
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Bedroom bedroom = new Bedroom(
+                "Master Bedroom",
+                new Wall("North"),
+                new Wall("South"),
+                new Wall("East"),
+                new Wall("West"),
+                new Ceiling(250, PaintColor.WHITE),
+                new Bed("Modern", 2, 60, 1, 1),
+                new Lamp(LampType.NEON, true, 5),
+                new Wardrobe(200, 220, 85.5),
+                new Carpet(300, 200, PaintColor.GREEN)
+        );
+
+        bedroom.getLamp().turnOn();
+        bedroom.getBed().make();
+        bedroom.getCarpet().lying();
+        bedroom.getWardrobe().add();
     }
 }
